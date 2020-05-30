b226175463b50e92e68506325981325e465ea9d3
Roodney Moraes <roodneysouza@gmail.com>
2020-05-29 22:39:41 -0300

    Aprendendo sobre verbos e requisições com insomnia

--- src/index.js
+++ src/index.js
@@ -4,10 +4,41 @@ const express = require('express');
 // INICIANDO FUNÇÃO
 const app = express();
 
+
 // CONFIGURANDO ROTA
-app.get('/', (request, response) => {
-    return response.json({message : 'Hello GoStack'});
-})
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 9',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 9',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
 
 // CONFIGURANDO PORTA
 app.listen(3333, () => {
